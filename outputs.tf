872d8d11838384a30922a0991b581acd21141b6a
Rachel Sharp <rachel@hashicorp.com>
2020-11-09 18:28:44 -0500

    remove eks

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.eks.cluster_endpoint
-}
-  
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = local.cluster_name
-}
