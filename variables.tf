7d9a96bf8c737cdeb1b091fb962dd742441975fb
Rachel Sharp <rachel@hashicorp.com>
2020-11-09 13:38:21 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "datadog_api_key" {
-  type = string
-  description = "Datadog API Key"
-}
-
-variable "datadog_app_key" {
-  type = string
-  description = "Datadog Application Key"
-}
-
 variable "application_name" {
   type        = string
   description = "Application Name"
