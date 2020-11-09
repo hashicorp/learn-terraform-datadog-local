719481c9a5f9208b4acc31b51f7b8901071b7691
Rachel Sharp <rachel@hashicorp.com>
2020-11-09 13:24:25 -0500

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    datadog = {
+      source  = "datadog/datadog"
+      version = "~> 2.12"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 1.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 1.11"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 2.52"
+    }
+  }
+
+  required_version = "~> 0.13.5"
+}
