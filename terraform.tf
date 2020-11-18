f1e421b6952d1eca2a1445de4152ab708a04b009
Rachel Sharp <rachel@hashicorp.com>
2020-11-18 09:29:59 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,21 +2,20 @@ terraform {
   required_providers {
     datadog = {
       source  = "datadog/datadog"
-      version = "~> 2.12"
+      version = "~> 2.16.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 1.2"
+      version = "~> 1.3.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 1.11"
+      version = "~> 1.13.3"
     }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.14"
     }
   }
-
-  required_version = "~> 0.13"
+  required_version = "~> 0.13.5"
 }
