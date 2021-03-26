980f7103aa30945ebbdba7f10a13878720c4f935
Robin Norwood <rnorwood@hashicorp.com>
2021-03-26 16:01:13 -0500

    Update k8s authentication method

--- kubernetes.tf
+++ kubernetes.tf
@@ -20,11 +20,18 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
-  load_config_file = "false"
-  host             = data.aws_eks_cluster.cluster.endpoint
-  token            = data.aws_eks_cluster_auth.cluster.token
-
+  host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.aws_eks_cluster.cluster.name
+    ]
+  }
 }
 
 resource "kubernetes_namespace" "beacon" {
