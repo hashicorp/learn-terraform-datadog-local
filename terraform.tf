5b7682f28ba30172ff9e7a0d5893f5b5dd108667
Brian McClain <brianmmcclain@gmail.com>
2023-01-19 11:34:10 -0500

    Bump to latest versions, up minimum Terraform version

--- terraform.tf
+++ terraform.tf
@@ -2,22 +2,22 @@ terraform {
   required_providers {
     datadog = {
       source  = "datadog/datadog"
-      version = "~> 2.24.0"
+      version = "~> 3.20.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.0.3"
+      version = "~> 2.8.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0.3"
+      version = "~> 2.16.1"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.34"
+      version = "~> 4.50.0"
     }
   }
-  required_version = "~> 0.13"
+  required_version = ">= 1.1.0"
 }
 
 provider "aws" {
