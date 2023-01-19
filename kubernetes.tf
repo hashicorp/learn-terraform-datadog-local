97c2ec12d0894cc1da488fbe482329f33ea53e26
Brian McClain <brianmmcclain@gmail.com>
2023-01-19 11:34:59 -0500

    Reference cluster name rather than ID to reflect AWS provider updates, up auth API version

--- kubernetes.tf
+++ kubernetes.tf
@@ -12,18 +12,14 @@ data "terraform_remote_state" "eks" {
 
 # Retrieve EKS cluster configuration
 data "aws_eks_cluster" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.cluster_id
+  name = data.terraform_remote_state.eks.outputs.cluster_name
 }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
     args = [
       "eks",
